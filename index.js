4ce05b4c9e5f8b1bbe125de3e159fcfd6d0f37fa
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-16 20:27:38 +0530

    MongoDB added for memory persistance

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const db = require("./config/mongoose");
+const Contact = require("./models/contact");
 const port = 8000;
 
 app.set("view engine", "ejs");
@@ -11,28 +12,51 @@ app.use(express.static("assets"));
 
 app.get("/", function (req, res) {
   //   console.log(req);
-  return res.render("home", {
-    title: "My Contact Page",
-    contact_list: contactList,
+
+  Contact.find({}, function (err, contacts) {
+    if (err) {
+      // console.log("")
+      console.log("Error in creating contact!");
+      return;
+    } else {
+      return res.render("home", {
+        title: "My Contact Page",
+        contact_list: contacts,
+      });
+    }
   });
+  // return res.render("home", {
+  //   title: "My Contact Page",
+  //   contact_list: contactList,
+  // });
 });
 
 //delete contact
 
-var contactList = [];
-app.get("/delete-contact/:phone", function (req, res) {
+app.get("/delete-contact/:id", function (req, res) {
   // console.log(req.params);
-  let phone = req.params.phone;
-  let ind = contactList.findIndex((contact) => contact.phone == phone);
-  if (ind != -1) {
-    contactList.splice(ind, 1);
-  }
+  let id = req.params.id;
+  console.log(id);
+  Contact.findByIdAndDelete(id, function (err) {
+    if (err) {
+      console.log("Error in deleting contact");
+      return;
+    }
+  });
   return res.redirect("back");
 });
 app.post("/create", function (req, res) {
   // console.log(req.body);
-  contactList.push(req.body);
-  return res.redirect("back");
+  // contactList.push(req.body);
+  Contact.create(req.body, function (err, newcontact) {
+    if (err) {
+      console.log("Error in creating contact!");
+      return;
+    } else {
+      console.log("***********", newcontact);
+      return res.redirect("back");
+    }
+  });
 });
 
 app.listen(port, function (err) {
